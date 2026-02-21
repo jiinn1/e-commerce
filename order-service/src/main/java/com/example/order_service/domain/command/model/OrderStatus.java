41d898af3a2178c640fec1b7b0fd71ac5bbfadb5
Jinn <lxqd@wp.pl>
2026-02-21 15:29:47 +0100

    init project

---
+++ order-service/src/main/java/com/example/order_service/domain/command/model/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.example.order_service.domain.command.model;
+
+public enum OrderStatus {
+    CREATED,
+    SHIPPED,
+    CANCELLED,
+    PAID,
+    COMPLETED
+}
---
+++ order-service/src/test/java/com/example/order_service/domain/command/model/OrderTest.java
@@ -0,0 +1,7 @@
+package com.example.order_service.domain.command.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+}
